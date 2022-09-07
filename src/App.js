68d6d6c8eb15b08b31ee89507ecc2aee681d291c
Junaid Malik <junmalik109@gmail.com>
2022-09-07 16:25:32 +0100

    App config and basic styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
+const App = () =>  {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+
+  const alanKey = 'af3fda033821aefd44baa860b76216b62e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+  useEffect(() => {
+    alanBtn({
+      key: apiKey,
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+        }
+      },
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React News App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   
       </header>
     </div>
   );
